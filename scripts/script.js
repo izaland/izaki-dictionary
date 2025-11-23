977b30f65df7e8cee3d04b20d2d93bdc10511d02
GrumpyMusician <theverygrumpymusician@gmail.com>
2025-11-22 20:24:54 -0600

    fixed stupid error hopefully

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ document.getElementById("navbar").innerHTML = `
             <div class="navbar-item has-dropdown is-hoverable">
                 <a class="navbar-link">Byakuzhi</a>
                 <div class="navbar-dropdown">
-                    <a class="navbar-item" src = "/izaki-dictionary/pages/byakuzhi/lookup.html">
+                    <a class="navbar-item" href = "/izaki-dictionary/pages/byakuzhi/lookup.html">
                         Lookup
                     </a>
                     <a class="navbar-item">
